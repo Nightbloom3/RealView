27e3a3df65be2e358d8cd76288c4cc548a7afa98
Nightbloom3 <Blom.kasper@gmail.com>
2023-08-21 10:59:20 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload
         </p>
         <a
           className="App-link"
@@ -18,8 +18,15 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div>
+        <p>dwadawdadawdwa</p>
+      </div>
+
+
     </div>
   );
 }
 
 export default App;
+
