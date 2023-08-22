0ecf28606c7d3c05dde7f8916794f9efb0b67f84
Nikolaj Larsen <56347460+Nearial@users.noreply.github.com>
2023-08-22 12:07:43 +0200

    First chart test

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,33 @@
-export function Home(){
-    return <h1>Home</h1>
+import BarChart from "../Charts/BarChart";
+import { useState, useEffect } from "react";
+import { TestData } from "../Charts/Data/TestData";
+
+export function Home() {
+  const [testData, setTestData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "",
+        data: [],
+      },
+    ],
+  });
+
+  useEffect(() => {
+    setTestData({
+      labels: TestData.map((data) => data.year),
+      datasets: [
+        {
+          label: "# Houses Sold",
+          data: TestData.map((data) => data.HousesSold),
+        },
+      ],
+    });
+  }, []);
+
+  return (
+    <div>
+      <BarChart ChartData={testData} />
+    </div>
+  );
 }
