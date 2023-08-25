b3c71dbd27eb51ef1574b215f86ad69597d9a97f
Nearial <nikolaj.larsen@sni-elteknik.dk>
2023-08-25 10:08:37 +0200

    Small fix for Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import { Routes, Route } from "react-router-dom";
 import FrontPage from "./Pages/FrontPage/FrontPage";
 import MyProfilePage from "./Pages/MyProfilePage";
+import Header from "./Pages/Header";
+import Navigation from "./Pages/Navigation";
+import Footer from "./Pages/Footer";
+
 
 function App() {
   return (
     <div>
-      <div className="App">
-        <FrontPage />
+      <div className="Header">
+      <Header />
+      </div>
+
+
+      <div className="Navigation">
+      <Navigation />
       </div>
 
       {/* Element - hvilken page den navigere til --- > path det man kan skrive i URL */}
       <Routes>
+        <Route path="/" element={<FrontPage/>}/>
         <Route path="/MyProfilePage" element={<MyProfilePage />} />
       </Routes>
+
+      <div className="Footer">
+      <Footer />
+      </div>
+
     </div>
   );
 }
--- src/Pages/FrontPage/FrontPage.jsx
+++ src/Pages/FrontPage/FrontPage.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import Header from "../Header";
-import Navigation from "../Navigation";
 import FrontPageContent from "./FrontPageContent";
-import Footer from "../Footer";
 import "./FrontPage.css";
 
 function FrontPage() {
   return (
     <div>
       <div className="FrontPage">
-        <Header />
-        <Navigation />
         <FrontPageContent />
-        <Footer />
       </div>
     </div>
   );
