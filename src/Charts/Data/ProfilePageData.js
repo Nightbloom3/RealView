36497fcabce586502a664cedc0ef5fdd05cdfffc
Nikolaj Larsen <56347460+Nearial@users.noreply.github.com>
2023-08-29 10:48:33 +0200

    V1 of Comparison Chart with Select Boxes

--- src/Charts/Data/ProfilePageData.js
+++ src/Charts/Data/ProfilePageData.js
@@ -1,61 +1,60 @@
-function Percentage (percentageValue, value) {
-    return (value / 100) * percentageValue;
+function Percentage(percentageValue, value) {
+  return (value / 100) * percentageValue;
 }
 
 export const ProfilePageData = [
-    {
-      id: 1,
-      year: 2001,
-      HousesAcquired: 207,
-      HousesSold: 430,
-      HousesAvgPriceSold: 1700000,
-      HousesAvgEarnings: Percentage(4, 1700000),
-      HousesAvgPriceChange: -340000
-    },
-    {
-      id: 2,
-      year: 2002,
-      HousesAcquired: 136,
-      HousesSold: 234,
-      HousesAvgPriceSold: 1840000,
-      HousesAvgEarnings: Percentage(5, 1840000),
-      HousesAvgPriceChange: -325000
-    },
-    {
-      id: 3,
-      year: 2003,
-      HousesAcquired: 86,
-      HousesSold: 67,
-      HousesAvgPriceSold: 1760000,
-      HousesAvgEarnings: Percentage(3.5, 1760000),
-      HousesAvgPriceChange: -265000
-    },
-    {
-      id: 4,
-      year: 2004,
-      HousesAcquired: 123,
-      HousesSold: 122,
-      HousesAvgPriceSold: 1670000,
-      HousesAvgEarnings: Percentage(4, 1670000),
-      HousesAvgPriceChange: -380000
-    },
-    {
-      id: 5,
-      year: 2005,
-      HousesAcquired: 155,
-      HousesSold: 237,
-      HousesAvgPriceSold: 18800000,
-      HousesAvgEarnings: Percentage(5.5, 18800000),
-      HousesAvgPriceChange: -235000
-    },
-    {
-      id: 6,
-      year: 2006,
-      HousesAcquired: 56,
-      HousesSold: 82,
-      HousesAvgPriceSold: 1760000,
-      HousesAvgEarnings: Percentage(4.5, 1760000),
-      HousesAvgPriceChange: -255000
-    },
-  ];
-  
+  {
+    id: 1,
+    year: 2001,
+    HousesAcquired: 207,
+    HousesSold: 430,
+    HousesAvgPriceSold: 1700000,
+    HousesAvgEarnings: Percentage(4, 1700000),
+    HousesAvgPriceChange: -340000,
+  },
+  {
+    id: 2,
+    year: 2002,
+    HousesAcquired: 136,
+    HousesSold: 234,
+    HousesAvgPriceSold: 1840000,
+    HousesAvgEarnings: Percentage(5, 1840000),
+    HousesAvgPriceChange: -325000,
+  },
+  {
+    id: 3,
+    year: 2003,
+    HousesAcquired: 86,
+    HousesSold: 67,
+    HousesAvgPriceSold: 1760000,
+    HousesAvgEarnings: Percentage(3.5, 1760000),
+    HousesAvgPriceChange: -265000,
+  },
+  {
+    id: 4,
+    year: 2004,
+    HousesAcquired: 123,
+    HousesSold: 122,
+    HousesAvgPriceSold: 1670000,
+    HousesAvgEarnings: Percentage(4, 1670000),
+    HousesAvgPriceChange: -380000,
+  },
+  {
+    id: 5,
+    year: 2005,
+    HousesAcquired: 155,
+    HousesSold: 237,
+    HousesAvgPriceSold: 1880000,
+    HousesAvgEarnings: Percentage(5.5, 1880000),
+    HousesAvgPriceChange: -235000,
+  },
+  {
+    id: 6,
+    year: 2006,
+    HousesAcquired: 56,
+    HousesSold: 82,
+    HousesAvgPriceSold: 1760000,
+    HousesAvgEarnings: Percentage(4.5, 1760000),
+    HousesAvgPriceChange: -255000,
+  },
+];
--- src/Pages/MyProfilePage/MyProfilePageContent.jsx
+++ src/Pages/MyProfilePage/MyProfilePageContent.jsx
@@ -4,86 +4,110 @@ import { ProfilePageData } from "../../Charts/Data/ProfilePageData";
 import { Chart as ChartJS } from "chart.js/auto";
 // ^^^^^^ Må ikke slettes - ellers virker siden ikke ^^^^^^^--- import { Chart as ChartJS } from "chart.js/auto";
 
-export default function MyProfilePageContent() {
+// Reusable component for the select box
+function SelectBox({ onChange, options, defaultValue }) {
+  return (
+    <div className="SelectBox">
+      <select id="selectChoices" onChange={onChange} value={defaultValue}>
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
 
-    const ChartStylingWidth = 500; // Set your desired width
-    const ChartStylingHeight = 500; // Set your desired height
-    const ComparisonStyling = {
-        MaintainAspectRatio: true,
-        responsive: true,
-        indexAxis: "y",
-        scales: {
-        xAxes: {
-            max: 500,
-        },
-    },
+export default function MyProfilePageContent() {
+  //Options for the chart
+  const ChartStylingWidth = 500; // Set your desired width
+  const ChartStylingHeight = 500; // Set your desired height
+  const ComparisonStyling = {
+    MaintainAspectRatio: true,
+    responsive: true,
+    indexAxis: "y",
   };
 
-    const [profileData, setProfileData] = useState({
-        labels: ProfilePageData.map((data) => data.year),
-        datasets: [
-            {
-                label: "# Houses Sold",
-                data: ProfilePageData.map((data) => data.HousesSold)
-            },
-            {
-                label: "Avg Earnings",
-                data: ProfilePageData.map((data) => data.HousesAcquired)
-            }
-        ]
-    })
+  const [selectChoice1, setSelectChoice1] = useState("HousesSold");
+  const [selectText1, setSelectText1] = useState("Houses Sold");
+  const [selectChoice2, setSelectChoice2] = useState("HousesAcquired");
+  const [selectText2, setSelectText2] = useState("Houses Acquired");
 
-    const [selectChoice, setSelectChoice] = useState("")
+  const [profileData, setProfileData] = useState({
+    labels: ProfilePageData.map((data) => data.year),
+    datasets: [
+      {
+        label: selectText1,
+        data: ProfilePageData.map((data) => data[selectChoice1]),
+      },
+      {
+        label: selectText2,
+        data: ProfilePageData.map((data) => data[selectChoice2]),
+      },
+    ],
+  });
 
-    useEffect(() => {
-        if (selectChoice) {
-            const dynamicKey = selectChoice; // Use the selected value as the key
-            const updatedData = {
-                ...profileData,
-                datasets: profileData.datasets.map((dataset) => {
-                    if (dataset.label === "# Houses Sold" || dataset.label === "Avg Earnings") {
-                        // Keep the existing datasets
-                        return dataset;
-                    }
-                    return {
-                        ...dataset,
-                        label: selectChoice, // Update the label
-                        data: ProfilePageData.map((data) => data[dynamicKey]),
-                    };
-                }),
-            };
-            setProfileData(updatedData);
-        }
-    }, [selectChoice, profileData]); // Add profileData as a dependency to avoid stale data
+  useEffect(() => {
+    setProfileData({
+      labels: ProfilePageData.map((data) => data.year),
+      datasets: [
+        {
+          label: selectText1,
+          data: ProfilePageData.map((data) => data[selectChoice1]),
+        },
+        {
+          label: selectText2,
+          data: ProfilePageData.map((data) => data[selectChoice2]),
+        },
+      ],
+    });
+  }, [selectChoice1, selectChoice2, selectText1, selectText2]); // State variable dependencies
 
-    function HandleChoiceChange(e) {
-        setSelectChoice(e.target.value)
-    }
+  function HandleChoiceChange1(e) {
+    const selectedIndex = e.target.selectedIndex;
+    setSelectChoice1(e.target.value);
+    setSelectText1(e.target.options[selectedIndex].label);
+  }
 
-    return (
-        <div>
-            <div className="Chart">
-            <ComparisonChart
-            ChartData={profileData}
-            width={ChartStylingWidth}
-            height={ChartStylingHeight}
-            StylingOptions={ComparisonStyling}
-            />
-            </div>
+  function HandleChoiceChange2(e) {
+    const selectedIndex = e.target.selectedIndex;
+    setSelectChoice2(e.target.value);
+    setSelectText2(e.target.options[selectedIndex].label);
+  }
 
-            <div className="SelectBox">
-                <p>Compare houses Sold to:</p>
-                <select id="selectChoices" onChange={HandleChoiceChange}>
-                    <option value="HousesAcquired"> Houses Acquired</option>
-                    <option value="HousesAvgPriceSold">Average sale price of houses</option>
-                    <option value="HousesAvgEarnings">Average earnings on house sales</option>
-                    <option value="HousesAvgPriceChange">Average price change on houses</option>
-                </select>
+  const selectOptions = [
+    { label: "Houses Sold", value: "HousesSold" },
+    { label: "Houses Acquired", value: "HousesAcquired" },
+    { label: "Average sale price of houses", value: "HousesAvgPriceSold" },
+    { label: "Average earnings on house sales", value: "HousesAvgEarnings" },
+    { label: "Average price change on houses", value: "HousesAvgPriceChange" },
+  ];
 
-            </div>
+  return (
+    <div>
+      <div className="Chart">
+        <ComparisonChart
+          ChartData={profileData}
+          width={ChartStylingWidth}
+          height={ChartStylingHeight}
+          StylingOptions={ComparisonStyling}
+        />
+      </div>
 
-        </div>
+      <SelectBox
+        onChange={HandleChoiceChange1}
+        options={selectOptions}
+        defaultValue={selectChoice1}
+      ></SelectBox>
 
-    )
+      <p>Compare to:</p>
 
+      <SelectBox
+        onChange={HandleChoiceChange2}
+        options={selectOptions}
+        defaultValue={selectChoice2}
+      ></SelectBox>
+    </div>
+  );
 }
