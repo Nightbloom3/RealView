622329aaef05409d2bf0ad6951f5c7f1382cb57c
Nearial <nikolaj.larsen@sni-elteknik.dk>
2023-09-05 14:07:24 +0200

    Added features to the table

--- src/Pages/MarketReport/Data/Columns.js
+++ src/Pages/MarketReport/Data/Columns.js
@@ -1,38 +1,58 @@
-export const COLUMNS = [
-    {
-        Header: "ID",
-        accessor: "id",
-    },
+import _ from "lodash"
+import { MarketReportData } from "./MarketReportData";
+
+export const columns = [
     {
         Header: "Realtor",
+        Footer: "Total",
         accessor: "realtor",
     },
     {
-        Header: "Houses for Sale",
+        Header: <div style={{ whiteSpace: "pre-line"}}> Number of<br />Houses</div>,
         accessor: "housesForSale",
+        id:"housesForSale",
+        Footer: <span>{_.sum(_.map(MarketReportData, d => d.housesForSale))}</span>
     },
     {
-        Header: "Market Share",
+        Header: <div style={{ whiteSpace: "pre-line"}}> Market<br />Share</div>,
         accessor: "marketShare",
+        Cell: ({ value}) => { return value, value + ' %' },
+        id:"marketShare",
+        Footer: <span>{_.sum(_.map(MarketReportData, d => d.marketShare)) + " %"}</span>
     },
     {
-        Header: "Avg Price per M2",
+        Header: <div style={{ whiteSpace: "pre-line"}}> Average<br />Price</div>,
         accessor: "avgPricePerM2",
+        Cell: ({ value}) => { return value, value + ' m²' },
+        id:"avgPricePerM2",
+        Footer: <span>{_.mean(_.map(MarketReportData, d => d.avgPricePerM2)).toFixed(0) + " m²"}</span>
     },
     {
-        Header: "Avg Size in M2",
+        Header: <div style={{ whiteSpace: "pre-line"}}> Average<br />Size</div>,
         accessor: "avgSizeInM2",
+        Cell: ({ value}) => { return value, value + ' m²' },
+        id:"avgSizeInM2",
+        Footer: <span>{_.mean(_.map(MarketReportData, d => d.avgSizeInM2)).toFixed(0) + " m²"}</span>
     },
     {
-        Header: "Avg time listed in days",
+        Header: <div style={{ whiteSpace: "pre-line"}}> Average<br />Time Listed</div>,
         accessor: "avgTimeListedInDays",
+        Cell: ({ value}) => { return value, value + ' days' },
+        id:"avgTimeListedInDays",
+        Footer: <span>{_.mean(_.map(MarketReportData, d => d.avgTimeListedInDays)).toFixed(0) + " days"}</span>
     },
     {
-        Header: "Price-reduced House Percentage",
+        Header: <div style={{ whiteSpace: "pre-line"}}> Price<br />Reduction Rate</div>,
         accessor: "priceReducedHousePercentage",
+        Cell: ({ value}) => { return value, value + ' %' },
+        id:"priceReducedHousePercentage",
+        Footer: <span>{_.mean(_.map(MarketReportData, d => d.priceReducedHousePercentage)).toFixed(2) + " %"}</span>
     },
     {
-        Header: "Avg Percentage Price Reduction",
+        Header: <div style={{ whiteSpace: "pre-line"}}> Average<br />Price Reduction</div>,
         accessor: "avgPercentagePriceReduction",
+        Cell: ({ value}) => { return value, value + ' %' },
+        id:"avgPercentagePriceReduction",
+        Footer: <span>{_.mean(_.map(MarketReportData, d => d.avgPercentagePriceReduction)).toFixed(2) + " %"}</span>
     },   
 ]
--- src/Pages/MarketReport/MarketReportContent.jsx
+++ src/Pages/MarketReport/MarketReportContent.jsx
@@ -1,52 +1,99 @@
 import { useMemo } from "react";
-import { useTable } from "react-table"
+import { useTable, useSortBy } from "react-table"
 import { MarketReportData } from "./Data/MarketReportData";
-import { COLUMNS } from "./Data/Columns";
+import { columns } from "./Data/Columns";
+import { CheckBox } from "./Data/CheckBox";
 
 export default function MarketReportContent() {
 
-    const tableColumns = useMemo(() => COLUMNS, []);
     const tableData = useMemo(() => MarketReportData, []);
+    const tableColumns = useMemo(() => columns, []);
 
-    const tableInstance = useTable({ columns: tableColumns, data: tableData });
-
-    const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = tableInstance
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        footerGroups,
+        rows,
+        prepareRow,
+        allColumns,
+        getToggleHideAllColumnsProps
+    } = useTable({
+        columns: tableColumns,
+        data: tableData
+    },
+        useSortBy)
 
     return (
         <div className="MarketReport_Content_Grid-container">
-            <div className="FormDiv">
-            <table {...getTableProps()}>
-                <thead>
-                    {headerGroups.map((headerGroup) => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <th {...column.getHeaderProps()}>
-                                    {column.render("Header")}
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map((cell) => {
-                                    return (
-                                        <td {...cell.getCellProps()}>
-                                            {cell.render("Cell")} {/* Note the capitalized C, its quite important otherwise you'll enjoy plenty of rendering issues*/}
-                                        </td>
-                                    )
-                                })}
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-            </div>
             <div className="ChartDiv">
 
+            </div>
+
+            <div className="FormDiv">
+                <>
+                <div className="CheckBoxDiv">
+                    <div>
+                        <label>
+                            <CheckBox {...getToggleHideAllColumnsProps()} name="Show/Hide Table" />
+                            Show/Hide Table
+                        </label>
+                    </div>
+                    {
+                        allColumns.map(column => (
+                            <div key={column.id}>
+                                <label>
+                                    <input type="checkbox" {...column.getToggleHiddenProps()}/>
+                                    {column.Header}
+                                </label>
+                            </div>
+                        ))
+                    }
+                </div>
+                <table {...getTableProps()}>
+                    <thead>
+                        {headerGroups.map((headerGroup) => (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map((column) => (
+                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                        {column.render("Header")}
+                                        <span>
+                                            {column.isSorted ? (column.isSortedDesc ? ' ↓' : ' ↑') : ''}
+                                        </span>
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+                        {rows.map((row) => {
+                            prepareRow(row)
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map((cell) => {
+                                        return (
+                                            <td {...cell.getCellProps()}>
+                                                {cell.render("Cell")} {/* Note the capitalized C, its quite important otherwise you'll enjoy plenty of rendering issues*/}
+                                            </td>
+                                        )
+                                    })}
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                    <tfoot>
+                        {footerGroups.map(footerGroup => (
+                            <tr {...footerGroup.getFooterGroupProps()}>
+                                {footerGroup.headers.map(column => (
+                                    <td {...column.getFooterProps()}>
+                                        {column.render("Footer")}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tfoot>
+                </table>
+                </>
             </div>
         </div>
     )
