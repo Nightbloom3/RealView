edd0d3aa1059bc2bdccb11f9f399498d297a3c3a
Nightbloom3 <Blom.kasper@gmail.com>
2023-09-13 14:12:49 +0200

    Notes for MapComponet

--- src/Pages/FindMyRealtor/MapComponet.jsx
+++ src/Pages/FindMyRealtor/MapComponet.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
-//npm install react-leaflet leaflet
 import 'leaflet/dist/leaflet.css';
-// import Freedraw, {  CREATE,  EDIT,  DELETE,  APPEND,  ALL } from "react-leaflet-freedraw";
 import "./mapComponet.css";
 
 function MapComponet({ onSelectCity }) {
+
+  // A custom hook that listens to the map's click event
+  // so if a click occurs, it will registere where.
+  function ClickListener() {
+    useMapEvents({
+      click: handleMapClick
+    });
+    return null;
+  }
+
+  // based on the click event / mouse click
+  // we are able to extract the latitude and longitude
   const handleMapClick = async (event) => {
     const lat = event.latlng.lat;
     const lng = event.latlng.lng;
 
+    // Which are used here to do a "reverse geocoding" call to openstreetmap API
+    // Makeing it possible to get a city name based on the latitude and longitude
     try {
         const response = await fetch(
           `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`
@@ -23,15 +35,8 @@ function MapComponet({ onSelectCity }) {
       }
     };
 
-  // A custom hook that listens to the map's click event
-  function ClickListener() {
-    useMapEvents({
-      click: handleMapClick
-    });
-    return null;
-  }
-
-  //might be possible to extract the mapContainer from the div and create a external CSS file
+  // Map Container is just to determine where on the map i should focus when render
+  // and the size  and zoom
   return (
     <div className='mapComponetDiv'>
       <button>Free Draw knap</button>
@@ -41,14 +46,15 @@ function MapComponet({ onSelectCity }) {
       zoom={7}
       style={{ height: '600px', width: '100%' }}
     >
-      <ClickListener />
+      {/* makes it possible to have the mouse click */}
+      <ClickListener/>
+      {/* The tileLayer is what map we use, so leaflet is the component to create actions and openstreetmap - is the map itself */}
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
     </MapContainer>
     </div>
-
   );
 }
 
