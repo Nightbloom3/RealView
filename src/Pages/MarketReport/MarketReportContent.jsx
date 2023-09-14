11979e8bc15424d0fb979e55bdfbb2000c97091e
Nearial <nikolaj.larsen@sni-elteknik.dk>
2023-09-14 11:47:36 +0200

    CSS fix for MarketReport

--- src/Pages/MarketReport/MarketReportContent.jsx
+++ src/Pages/MarketReport/MarketReportContent.jsx
@@ -132,7 +132,7 @@ export default function MarketReportContent() {
                   </div>
                 ))}
               </div>
-              <table {...getTableProps()}>
+              <table className="MarketReportTable" {...getTableProps()}>
                 <thead>
                   {headerGroups.map((headerGroup) => (
                     <tr {...headerGroup.getHeaderGroupProps()}>
