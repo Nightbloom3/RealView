a2936455cde6dc440e1ebc881b5d528e077df758
Nearial <nikolaj.larsen@sni-elteknik.dk>
2023-09-22 12:50:48 +0200

    some tidying up of the MarketReportContent

--- src/Pages/MarketReport/MarketReportContent.jsx
+++ src/Pages/MarketReport/MarketReportContent.jsx
@@ -6,7 +6,82 @@ import ScatterChart from "../../Components/Charts/ScatterChart";
 import DataTable from "../../Components/Tables/DataTable";
 import "chart.js/auto";
 import _ from "lodash";
-import { calculateNewValue } from "@testing-library/user-event/dist/utils";
+
+function generateRandomColor(count) {
+  const colors = [];
+  for (let i = 0; i < count; i++) {
+    const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    colors.push(color);
+  }
+  return colors;
+};
+
+function percentageCalc (percentage, whole) {
+  return ((percentage / 100) * whole);
+}
+
+function mergeRowsByRealtor(dataset) {
+  const mergedData = [];
+  const realtorDictionary = {};
+
+  dataset.forEach((row) => {
+    const realtor = row.realtor;
+
+    if (realtorDictionary[realtor]) {
+      const mergedRow = realtorDictionary[realtor];
+
+      //logic block for calculating the new percentage of houses that have their price reduced,
+      //has to take place before the new sum of houses are calculated otherwise it will mess with the formular
+      const currentRow = Math.round(percentageCalc(mergedRow.priceReducedHousePercentage, mergedRow.housesForSale))
+      const mergingRow = Math.round(percentageCalc(row.priceReducedHousePercentage, row.housesForSale))
+      const mergingHousesForSale = (mergedRow.housesForSale + row.housesForSale)
+      const mergedPercentage = parseFloat((((currentRow + mergingRow) / mergingHousesForSale) * 100).toFixed(2))
+
+      mergedRow.housesForSale += row.housesForSale;
+      mergedRow.avgPricePerM2 = (mergedRow.avgPricePerM2 + row.avgPricePerM2) / 2;
+      mergedRow.avgSizeInM2 = (mergedRow.avgSizeInM2 + row.avgSizeInM2) / 2;
+      mergedRow.avgTimeListedInDays = (mergedRow.avgTimeListedInDays + row.avgTimeListedInDays) / 2;
+      mergedRow.priceReducedHousePercentage = mergedPercentage
+
+      if (mergedRow.avgPercentagePriceReduction === 0 || row.avgPercentagePriceReduction === 0) {
+        mergedRow.avgPercentagePriceReduction += row.avgPercentagePriceReduction;
+      } 
+      else if (mergedRow.avgPercentagePriceReduction > 0 && row.avgPercentagePriceReduction > 0) {
+        mergedRow.avgPercentagePriceReduction = (mergedRow.avgPercentagePriceReduction + row.avgPercentagePriceReduction) / 2;
+      }
+
+      mergedRow.infoBlurb = row.infoBlurb;
+
+    } else {
+      const newRow = { ...row };
+      realtorDictionary[realtor] = newRow;
+    }
+  });
+  
+  // Push merged rows into the mergedData array
+  for (const realtor in realtorDictionary) {
+    mergedData.push(realtorDictionary[realtor]);
+  }
+
+  return mergedData;
+};
+
+function transformDataForScatterChart(dataset) {
+  return {
+    labels: dataset.map((data) => data.realtor),
+    datasets: [
+      {
+        label: "Average Time Listed / Average Price per M²",
+        data: dataset.map((data) => ({
+          x: data.avgTimeListedInDays,
+          y: data.avgPricePerM2,
+        })),
+        backgroundColor: generateRandomColor(dataset.length),
+        pointRadius: 4,
+      },
+    ],
+  };
+}
 
 export default function MarketReportContent() {
   const ChartStylingWidth = 800; // Set your desired width
@@ -43,21 +118,26 @@ export default function MarketReportContent() {
     },
   };
 
+  //Set function should be used in the future to hold whatever postal numbers are chosen by the user
   const [checkboxItems, setCheckboxItems] = useState(["3450", "3520"])
+  //useState to keep track of what checkboxes are checked
   const [checkedItems, setCheckedItems] = useState(checkboxItems.map(() => false));
+  //useState to keep track of what datasets are currently in use based on which checkboxes are checked,
+  //should also be refactored in the future when access to the db is given.
   const [currentDataset, setCurrentDataset] = useState([])
   const datasetsRef = useRef({
     dataset1: MarketReportData,
     dataset2: MarketReportData2,
   })
 
-
+  //onClick function for handling the changes in the checkbox
   const handleCheckboxClick = (index) => {
     const newCheckedItems = [...checkedItems];
     newCheckedItems[index] = !newCheckedItems[index];
     setCheckedItems(newCheckedItems);
   };
 
+  //useEffect to concat chosen datasets together before they are then merged later
   useEffect(() => {
     let newCurrentDataset = [];
     for (let i = 0; i < checkedItems.length; i++) {
@@ -74,93 +154,14 @@ export default function MarketReportContent() {
     });
   }, [checkedItems]);
 
- 
-  // a small function that picks a random hex color for the purpose of coloring the plots in the scatter plot
-  const randomColor = (count) => {
-    const colors = [];
-    for (let i = 0; i < count; i++) {
-      const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-      colors.push(color);
-    }
-    return colors;
-  };
-
   //small method for finding the length amount of colors neded to give the dataset
   //If given simply 1 color, lets say "red", then all the points would be red, if it were given
   //"red, blue" then it would alternate between red and blue for each point,
   //so to make sure each point has its own colour we need to assign all of them a color
   const numberOfDataPoints = currentDataset.length;
 
-  //Setting up data for chart, will need to make use of the set method and a useEffect once there is access to the DB
-  const scatterChartData = ({
-    labels: currentDataset.map((data) => data.realtor),
-    datasets: [
-      {
-        label: "Average Time Listed / Average Price per M²",
-        data: currentDataset.map((data) => ({
-          x: data.avgTimeListedInDays,
-          y: data.avgPricePerM2,
-        })),
-        backgroundColor: randomColor(numberOfDataPoints),
-        pointRadius: 4,
-      },
-    ],
-  });
-
   //useState for tracking whether or not the div holding the table is hidden or not
   const [isDivHidden, setIsDivHidden] = useState(false);
-
-  function percentageCalc (percentage, whole) {
-    return ((percentage / 100) * whole);
-  }
-
-  const mergeRowsByRealtor = (dataset) => {
-    const mergedData = [];
-    const realtorDictionary = {};
-  
-    dataset.forEach((row) => {
-      const realtor = row.realtor;
-  
-      if (realtorDictionary[realtor]) {
-        const mergedRow = realtorDictionary[realtor];
-
-        //logic block for calculating the new percentage of houses that have their price reduced,
-        //has to take place before the new sum of houses are calculated otherwise it will mess with the formular
-        const currentRow = Math.round(percentageCalc(mergedRow.priceReducedHousePercentage, mergedRow.housesForSale))
-        const mergingRow = Math.round(percentageCalc(row.priceReducedHousePercentage, row.housesForSale))
-        const mergingHousesForSale = (mergedRow.housesForSale + row.housesForSale)
-        const mergedPercentage = parseFloat((((currentRow + mergingRow) / mergingHousesForSale) * 100).toFixed(2))
-
-        mergedRow.housesForSale += row.housesForSale;
-        mergedRow.avgPricePerM2 = (mergedRow.avgPricePerM2 + row.avgPricePerM2) / 2;
-        mergedRow.avgSizeInM2 = (mergedRow.avgSizeInM2 + row.avgSizeInM2) / 2;
-        mergedRow.avgTimeListedInDays = (mergedRow.avgTimeListedInDays + row.avgTimeListedInDays) / 2;
-
-        mergedRow.priceReducedHousePercentage = mergedPercentage
-
-
-
-        if (mergedRow.avgPercentagePriceReduction === 0 || row.avgPercentagePriceReduction === 0) {
-          mergedRow.avgPercentagePriceReduction += row.avgPercentagePriceReduction;
-        } else if (mergedRow.avgPercentagePriceReduction > 0 && row.avgPercentagePriceReduction > 0) {
-          mergedRow.avgPercentagePriceReduction = (mergedRow.avgPercentagePriceReduction + row.avgPercentagePriceReduction) / 2;
-        }
-  
-        mergedRow.infoBlurb = row.infoBlurb;
-  
-      } else {
-        const newRow = { ...row };
-        realtorDictionary[realtor] = newRow;
-      }
-    });
-  
-    // Push merged rows into the mergedData array
-    for (const realtor in realtorDictionary) {
-      mergedData.push(realtorDictionary[realtor]);
-    }
-  
-    return mergedData;
-  };
 
   const mergedDataSet = mergeRowsByRealtor(currentDataset)
 
@@ -178,7 +179,7 @@ export default function MarketReportContent() {
 
       <div className="ChoiceDiv">
         <fieldset>
-          <legend>Choose a area</legend>
+          <legend>Select Postal Number(s)</legend>
 
           {checkboxItems.map((item, index) => (
             <div key={index}>
@@ -197,7 +198,7 @@ export default function MarketReportContent() {
 
       <div className="ChartDiv">
         <ScatterChart
-          ChartData={scatterChartData}
+          ChartData={transformDataForScatterChart(mergedDataSet)}
           width={ChartStylingWidth}
           height={ChartStylingHeight}
           StylingOptions={scatterChartStyling}
